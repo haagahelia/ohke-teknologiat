94a229ea555682cb035f5ecdf749abc20381cf98
Ohto Rainio <orainio@Ohtos-MBP.home>
2020-08-18 15:19:38 +0300

    tehtävät mukana

---
+++ 02_ohjelmistoarkkitehtuurit_ja_patternit/src/Reduxperiaatteet.js
@@ -0,0 +1,54 @@
+/*Tässä tehtävässä on tarkoitus rakentaa pieni scripti 
+värien lisäämiseen ja poistamiseen säiliöstä Reduxin periaatteiden mukaisesti. 
+Lopulta scriptin pitäisi siis tulosta "keltainen", "vihreä"
+Muista ajaa ensin src-kansiossa komento "npm init"
+Voit ajaa tämän tiedoston komennolla "node Reduxperiaatteet.js" tai "npm run build"
+*/
+
+//import Redux from 'redux' //ES6 syntaksilla voisi tehdä näin
+
+const Redux = require('redux')
+
+//Action 1
+function addColor(value){
+    return{
+        type: "", //FIXME
+        color: null //FIXME
+    };
+}
+
+//Action 2
+function removeColor(value){
+    return{
+        type: "", //FIXME
+        color: null //FIXME
+    };
+}
+
+//Pohja reducerille
+function favoriteColors(state, action){
+    if(state == undefined){
+        state = []
+    }
+
+    if(action.type === "ADD"){
+        return state.concat(action.color);
+    }else if(action.type === "REMOVE"){
+        return state.filter(function(item){ 
+            //selvitä tarvittaessa miten toimii funktionaalisen ohjelmoinnin filter-funktio
+            return null //FIXME
+        });
+    }else{
+        return state;
+    }
+}
+
+//Luodaan redux-säiliö ja käytetään sitä
+let store = Redux.createStore(favoriteColors);
+store.dispatch(addColor("sininen"));
+store.dispatch(addColor("keltainen"));
+store.dispatch(addColor("vihreä"));
+store.dispatch(removeColor("sininen"));
+
+console.log(store.getState()); //pitäisi tulla tuloste ['keltainen', 'vihreä']
+
