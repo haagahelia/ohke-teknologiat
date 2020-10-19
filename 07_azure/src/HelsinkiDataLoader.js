7550ab177285c3723d50e8f0047f87d4f6fb4ce9
koodiohto <orainio@gmail.com>
2020-10-19 13:02:00 +0300

    basic package.json

--- 07_azure/src/HelsinkiDataLoader.js
+++ 07_azure/src/HelsinkiDataLoader.js
@@ -1,3 +1,13 @@
 /**
- * Load data from Helsinki API
+ * Load data from Helsinki API and run to a CSV-file
  */
+const fetch = require('node-fetch');
+
+async function getEventsAsync() {
+    let response = await fetch('http://open-api.myhelsinki.fi/v1/events/');
+    let json = await response.json();
+    let events = json.data;
+    return events;
+}
+
+//module.exports = { getEventsAsync };
