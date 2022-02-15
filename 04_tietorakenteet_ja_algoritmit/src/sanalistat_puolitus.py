763e6cefbb27daaeaaef72291f0beec90f915de5
Teemu H <swd1tn002@users.noreply.github.com>
2022-02-15 14:53:11 +0200

    TRAK-tunnin esimerkkikoodit

--- 04_tietorakenteet_ja_algoritmit/src/sanalistat_puolitus.py
+++ 04_tietorakenteet_ja_algoritmit/src/sanalistat_puolitus.py
@@ -4,27 +4,42 @@ import time
 # n=94 110
 finnish = Path(
     'kotus-sanalista-v1/kotus-sanalista-suomi.txt').read_text().splitlines()
+finnish.sort()
+
 # n=102 401
 english = Path('/usr/share/dict/words').read_text().splitlines()
+english.sort()
+
+
+def binary_search(search: str, words: list) -> bool:
+    min = 0
+    max = len(words) - 1
+
+    while min <= max:
+        mid = (max + min) // 2
+        if search == words[mid]:
+            return True
+        elif search < words[mid]:
+            max = mid - 1
+        elif search > words[mid]:
+            min = mid + 1
+
+    return False
 
 
 def find_common(fi, en):
     common = []
 
-    # n**2
+    # n * log2(n)
     for word in fi:  # 94 110
-        if word in en:  # 102 401
+        if binary_search(word, en):  # log2(n)
             common.append(word)
 
     return common
 
 
-# find_common(finnish, english)
-
-"""
-for size in range(1_000, 13_000, 1_000):
+for size in range(20_000, 110_000, 20_000):
     start = time.perf_counter()
     find_common(finnish[:size], english[:size])
     end = time.perf_counter()
     print(f'{size}  {end-start}')
-"""
